e2bb73ac45e7581b7b2bfac5dca0c1eda6ac0ba7
David Renard <david.renard@the-rens.com>
2025-07-06 12:21:47 +0000

    Extensible entities

--- src/ZhorteinSeoTrackingBundle.php
+++ src/ZhorteinSeoTrackingBundle.php
@@ -9,33 +9,10 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Zhortein\SeoTrackingBundle\DependencyInjection\Compiler\DoctrineMappingPass;
-use Zhortein\SeoTrackingBundle\DependencyInjection\Compiler\ResolveTargetEntitiesPass;
 use Zhortein\SeoTrackingBundle\DependencyInjection\Extension\ZhorteinSeoTrackingExtension;
-use Zhortein\SeoTrackingBundle\Entity\PageCall;
-use Zhortein\SeoTrackingBundle\Entity\PageCallHit;
-use Zhortein\SeoTrackingBundle\Entity\PageCallHitInterface;
-use Zhortein\SeoTrackingBundle\Entity\PageCallInterface;
 
 class ZhorteinSeoTrackingBundle extends AbstractBundle
 {
-    public function prepend(ContainerBuilder $container): void
-    {
-        // Valeurs par défaut (en attendant que l’Extension injecte les vraies)
-        $config = $container->getExtensionConfig('zhortein_seo_tracker')[0] ?? [];
-
-        $pageCallClass = $config['page_call_class'] ?? PageCall::class;
-        $pageCallHitClass = $config['page_call_hit_class'] ?? PageCallHit::class;
-
-        $container->prependExtensionConfig('doctrine', [
-            'orm' => [
-                'resolve_target_entities' => [
-                    PageCallInterface::class => $pageCallClass,
-                    PageCallHitInterface::class => $pageCallHitClass,
-                ],
-            ],
-        ]);
-    }
-
     /**
      * @param array<int|string, mixed> $config
      *
@@ -54,7 +31,6 @@ class ZhorteinSeoTrackingBundle extends AbstractBundle
     {
         parent::build($container);
         $container->addCompilerPass(new DoctrineMappingPass());
-        $container->addCompilerPass(new ResolveTargetEntitiesPass());
     }
 
     public function getContainerExtension(): ?ExtensionInterface
